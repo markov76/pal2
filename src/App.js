d874d77e2131a44612c4cb83ce884b88fa84b45a
markov76 <101133846+markov76@users.noreply.github.com>
2022-10-15 23:30:35 +0300

    korjailtu

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
 
   const [loppuPiste, setLoppuPiste] = useState('')
 
-  const [container, setContainer] = useState([])
+  const [kontainer, setkontainer] = useState([])
 
-  const [finalPoint, setFinalPoint] = useState('')
+  const [lopetusPiste, setlopetusPiste] = useState('')
 
   useEffect(() => {
     fetchMe()
-  },[finalPoint])
+  },[lopetusPiste])
 
 
   
@@ -29,9 +29,9 @@ function App() {
     .then(response => {
       return response.json()
     })
-    //.then(response => console.log(response))
+    
     .then(data => {
-      setContainer(data.d)
+      setkontainer(data.d)
     })
     .catch(err => console.error(err));
   }  
@@ -41,7 +41,7 @@ function App() {
 
   const haeHandler = e => {
     e.preventDefault()
-    setFinalPoint(loppuPiste)
+    setlopetusPiste(loppuPiste)
   }
 
   return (
@@ -54,7 +54,7 @@ function App() {
 
         </form>
     <div className='element'>
-        {container.map((item,index) => {
+        {kontainer.map((item,index) => {
           return (
             <div key={index} className='element-div'>
             <img src={item.i.imageUrl} alt="" />  
